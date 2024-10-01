6d5f383e13d1a35a8b9b58edd42ca95676f3f838
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-10-01 13:03:38 +0000

    jungkook

--- PP1_5.py
+++ PP1_5.py
@@ -1,19 +1,46 @@
-
+'''
+  Lesson: Typecasting
+  Author: Helen Lin
+  Date Created: September 23, 2024
+  Date Last Modified: September 23, 2024
+'''
 def q1():
   #Write Assignment code here
+  num = input("Input an integer: ")
+  num = int(num)
+  num = num + 3
+  print(num)
 
 def q2():
   #Write Assignment code here
+  bum = input("Input a number: ")
+  bum = int(bum)
+  bum = bum + 4
+  bum = float(bum)
+  print (bum + 2)
 
 def q3():
   #Write Assignment code here
-
+  radius = input("Input a radius: ")
+  radius = float(radius)
+  area = (3.14 * radius) 
+  area = (area * area) 
+  print(area)
+  
 def q4():
   #Write Assignment code here
+  num = input("Input a number: ")
+  num = float(num)
+  num = (num * 12)
+  num = int(num)
+  print(num)
 
 def q5():
   #Write Assignment code here
-
+  var = input("Input an integer: ")
+  var = int(var)
+  var = var + 5
+  print (f"Your number + 5 is: {var}")
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
